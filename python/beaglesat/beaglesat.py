c7257237af13d309a0fa4f57f55203fb99a09520
Niko Visnjic <visnjic.niko@gmail.com>
2015-08-21 20:28:45 +0200

    added GPLv3 licence

--- python/beaglesat/beaglesat.py
+++ python/beaglesat/beaglesat.py
@@ -2,7 +2,22 @@
 BeagleSat
 Copyright 2015 - Niko Visnjic <self@nvisnjic.com>
 
-A library for controlling the BeagleSat CubeSat platform
+A library for controlling the BeagleSat CubeSat platform.
+
+    This file is part of BeagleSat.
+
+    BeagleSat is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BeagleSat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BeagleSat.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
